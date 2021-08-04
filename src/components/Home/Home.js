aabe93b57e28f77c7e47ec7b4d3750d20b920314
TilekSh1199 <tilek1199@gmail.cm>
2021-08-04 11:07:48 +0600

    filter

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import { Grid } from '@material-ui/core';
 import React from 'react';
 import Content from './Content';
+import Sidebar from './Sidebar';
 
 const Home = () => {
     return (
         <div style={{padding: '20px'}}>
             <Grid container spacing-md={3} spacing-sm={3}>
+                <Sidebar />
                 <Content/>
             </Grid>
         </div>
